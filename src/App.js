dc8009e855fc764a7089a900e6a0bfd716214026
qiuqianaw <qiuqianaw@163.com>
2022-06-16 20:07:05 +0800

    feature: state定义和使用

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
+// 组件状态 class component
+
 import React from "react";
-import "./App.css";
 
-// 函数组件
-function HelloFunctionComponent() {
-  const clickHandler = (e, msg) => {
-    // 阻止默认行为
-    e.preventDefault();
-    console.log(e);
-    console.log(msg);
+class TestComponent extends React.Component {
+  // 1. 定义组件状态
+  state = {
+    name: "qiuqianaw",
   };
-  return (
-    <div>
-      <div onClick={(e) => clickHandler(e, "there is a message")}>click me</div>
-    </div>
-  );
-}
+  // 3.修改状态
+  changeName = () => {
+    this.setState({
+      name: "qiuqian",
+    });
+  };
 
-// 类组件
-// 回调函数this指向当前组件实例对象
-class HelloClassComponent extends React.Component {
-  clickHandler = () => {
-    console.log("类组件中的事件被触发");
-  };
   render() {
-    return <div onClick={this.clickHandler}>this is a class Component</div>;
+    // 2.使用状态
+    return (
+      <div>
+        hello, {this.state.name}
+        <button onClick={this.changeName}>change name</button>
+      </div>
+    );
   }
 }
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <HelloFunctionComponent />
-        <HelloClassComponent />
-      </div>
-    </>
+    <div>
+      <TestComponent />
+    </div>
   );
 }
 
