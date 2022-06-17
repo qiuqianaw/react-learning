d363bee74c39ad3624dfa9fcb4fac13e42e9b860
qiuqianaw <qiuqianaw@163.com>
2022-06-17 11:03:16 +0800

    feature: 组件通信, parent componnet -> son component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React, { createRef } from "react";
-// 非受控组件
-// 通过手动操作dom的方式获取文本框值，文本框状态不再受state控制，通过原生dom获取输入框的值
-class Input extends React.Component {
-  msgRef = createRef();
-  getValue = () => {
-    console.log(this.msgRef.current.value);
+import React from "react";
+
+// 函数式 son component
+function SonFunction(props) {
+  // props对象存的是通过父组件传入的所有数据
+  return <div>函数子组件, {props.msg}</div>;
+}
+
+// 类式 son component
+class SonClass extends React.Component {
+  render() {
+    return <div>类子组件, {this.props.msg}</div>;
+  }
+}
+
+// parent component
+class App extends React.Component {
+  state = {
+    message: "this is a msg",
   };
   render() {
     return (
       <>
-        <input type="text" ref={this.msgRef} />
-        <button onClick={this.getValue}>点击获取输入框的值</button>
+        <div>
+          <SonFunction msg={this.state.message} />
+          <SonClass msg={this.state.message} />
+        </div>
       </>
     );
   }
-}
-
-function App() {
-  return (
-    <div>
-      <Input />
-    </div>
-  );
 }
 
 export default App;
