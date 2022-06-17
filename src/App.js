fee325f237d14073b411864f394673c949117352
qiuqianaw <qiuqianaw@163.com>
2022-06-17 11:44:39 +0800

    feature: 组件通信, parent componnet -> son component, number, String, bool, Array, Object, function, jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import React from "react";
 // 函数式 son component
 function SonFunction(props) {
   // props对象存的是通过父组件传入的所有数据
-  return <div>函数子组件, {props.msg}</div>;
+  return (
+    <>
+      <div>函数子组件, {props.msg}</div>
+      <div>
+        <ul>
+          {props.list.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      <div>{props.userInfo.name}</div>
+      <div>
+        <button onClick={props.getMsg}>触发父组件传入的函数</button>
+      </div>
+      <div>{props.child}</div>
+    </>
+  );
 }
 
 // 类式 son component
@@ -17,12 +33,27 @@ class SonClass extends React.Component {
 class App extends React.Component {
   state = {
     message: "this is a msg",
+    list: [1, 2, 3, 4, 5],
+    userInfo: {
+      name: "qiuqian",
+      gender: 1,
+      age: 18,
+    },
+  };
+  getMsg = () => {
+    console.log("function from parent component!");
   };
   render() {
     return (
       <>
         <div>
-          <SonFunction msg={this.state.message} />
+          <SonFunction
+            msg={this.state.message}
+            list={this.state.list}
+            userInfo={this.state.userInfo}
+            getMsg={this.getMsg}
+            child={<span>this is a span</span>}
+          />
           <SonClass msg={this.state.message} />
         </div>
       </>
