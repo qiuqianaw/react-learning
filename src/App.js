5538e00f022fadb43cac183d15b1aec884eaa6ff
qiuqianaw <qiuqianaw@163.com>
2022-06-23 14:05:34 +0800

    feature: PropTypes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { Button } from "antd";
-import "./App.css"
+import PropTypes from "prop-types";
+
+function Test({ list }) {
+  return (
+    <>
+      <div>
+        {list.map((item) => (
+          <p>{item}</p>
+        ))}
+      </div>
+    </>
+  );
+}
+Test.propTypes = {
+  // 定义校验规则
+  list: PropTypes.array.isRequired,
+};
 
 class App extends React.Component {
   state = {};
 
   render() {
     return (
-      <div className="container">
-        <Button type="primary">测试button</Button>
+      <div>
+        <Test list={[1, 2, 3, 4, 5]} />
       </div>
-    )
+    );
   }
 }
 
