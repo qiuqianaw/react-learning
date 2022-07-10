52077ca2c31030cb45b3b48fd72556c957ab14a5
qiuqianaw <qiuqianaw@163.com>
2022-07-10 17:08:48 +0800

    test: 7.10

--- src/App.js
+++ src/App.js
@@ -1,60 +1,94 @@
+import { Button, Input, Popconfirm, Table } from "antd";
 import React from "react";
+// import axios from "axios";
+import "./App.css";
 
-class Test extends React.Component {
-  // 如果数据是组件的状态，会影响视图，定义到state
-  // 如果数据不和视图绑定，定义普通实例属性
-  timer = null;
+const { Search } = Input;
+
+class App extends React.Component {
+  state = {
+    list: [],
+    colunms: [
+      {
+        title: "任务编号",
+        dataIndex: "id",
+        key: "id",
+      },
+      {
+        title: "任务名称",
+        dataIndex: "name",
+        key: "name",
+      },
+      {
+        title: "任务描述",
+        dataIndex: "des",
+        key: "des",
+      },
+      {
+        title: "操作",
+        dataIndex: "operate",
+        key: "operate",
+        render: (_, record) => {
+          <Popconfirm
+            title="Sure to delete?"
+            onConfirm={() => this.deleteHandler(record)}
+          >
+            <Button>Delete</Button>
+          </Popconfirm>;
+        },
+      },
+    ],
+  };
+  // 搜索
+  onSearch = (value) => {
+    console.log(value);
+  };
+  // 删除
+  deleteHandler = (record) => {};
+
+  // 加载列表
+  loadList = async () => {
+    // const res = await axios.get("http://localhost:3001/data");
+    const res = [
+      {
+        id: "1",
+        name: "hello",
+        des: "hello, world",
+      },
+      {
+        id: "2",
+        name: "Golang",
+        des: "Golang yyds",
+      },
+      {
+        id: "3",
+        name: "TypeScript",
+        des: "TypeScript yyds",
+      },
+    ];
+    this.setState({
+      list: res,
+    });
+  };
+
   componentDidMount() {
-    this.timer = setInterval(() => {
-      console.log("定时器开启");
-    }, 1000);
+    this.loadList();
   }
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-    // 清理定时器
-    clearInterval(this.timer);
-  }
+
   render() {
     return (
       <>
-        <div>test</div>
+        <div className="container">
+          <Search
+            placeholder="please input content"
+            allowClear
+            enterButton="Search"
+            size="large"
+            onSearch={this.onSearch}
+          />
+          <Table columns={this.state.colunms} dataSource={this.state.list} />
+        </div>
       </>
-    );
-  }
-}
-
-// 生命周期 类组件需要被实例化，才有生命周期
-// constructor => render(渲染ui 每次渲染都会触发) => componentDidMount(操作DOM，副作用)
-class App extends React.Component {
-  constructor() {
-    super();
-    console.log("constructor");
-  }
-  state = {
-    count: 0,
-    flag: true,
-  };
-  clickHandler = () => {
-    this.setState({ count: this.state.count + 1, flag: !this.state.flag });
-  };
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
-  componentDidUpdate() {
-    // 组件更新后(DOM渲染完毕)， 不要在里面调用setState
-    console.log("componentDidUpdate");
-  }
-
-  render() {
-    // 不要在里面调用setState
-    console.log("render");
-    return (
-      <div>
-        this is div
-        {/* 通过数据状态切换，让Test组件创建和销毁 */}
-        {(this, this.state.flag ? <Test /> : null)}
-        <button onClick={this.clickHandler}>{this.state.count}</button>
-      </div>
     );
   }
 }
