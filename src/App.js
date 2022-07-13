f8bdefcc395af8fbebf4aaa17953019f1e381149
qiuqianaw <qiuqianaw@163.com>
2022-07-13 18:03:50 +0800

    feature: useState

--- src/App.js
+++ src/App.js
@@ -1,115 +1,18 @@
-import { Button, Input, Popconfirm, Table } from "antd";
-import React from "react";
-// import axios from "axios";
-import "./App.css";
+// useState
+// 1. 倒入 useState
+// 2. 传入初始值
+// 3. [数据, 修改数据的方法]
+// 4. 使用，修改数据
 
-const { Search } = Input;
-
-class App extends React.Component {
-  state = {
-    colunms: [
-      {
-        title: "任务编号",
-        dataIndex: "id",
-        key: "id",
-      },
-      {
-        title: "任务名称",
-        dataIndex: "name",
-        key: "name",
-      },
-      {
-        title: "任务描述",
-        dataIndex: "des",
-        key: "des",
-      },
-      {
-        title: "操作",
-        render: (_, record) => (
-          <Popconfirm
-            title="sure to delete?"
-            onConfirm={() => this.deleteHandler(_, record)}
-          >
-            <Button>Delete</Button>
-          </Popconfirm>
-        ),
-      },
-    ],
-    list: [
-      {
-        id: "1",
-        name: "hello",
-        des: "hello, world",
-      },
-      {
-        id: "2",
-        name: "Golang",
-        des: "Golang yyds",
-      },
-      {
-        id: "3",
-        name: "TypeScript",
-        des: "TypeScript yyds",
-      },
-    ],
-  };
-  // 搜索
-  onSearch = (value) => {
-    console.log(value);
-  };
-  // 删除
-  deleteHandler = (_, record) => {
-    console.log("delete", _, record);
-    this.setState({
-      list: this.state.list.filter((item) => item.id !== record.id),
-    });
-  };
-
-  // 加载列表
-  loadList = async () => {
-    // const res = await axios.get("http://localhost:3001/data");
-    const res = [
-      {
-        id: "1",
-        name: "hello",
-        des: "hello, world",
-      },
-      {
-        id: "2",
-        name: "Golang",
-        des: "Golang yyds",
-      },
-      {
-        id: "3",
-        name: "TypeScript",
-        des: "TypeScript yyds",
-      },
-    ];
-    this.setState({
-      list: res,
-    });
-  };
-
-  componentDidMount() {
-    // this.loadList();
-  }
-
-  render() {
-    return (
-      <>
-        <div className="container">
-          <Search
-            placeholder="please input content"
-            allowClear
-            enterButton="Search"
-            size="large"
-            onSearch={this.onSearch}
-          />
-          <Table columns={this.state.colunms} dataSource={this.state.list} />
-        </div>
-      </>
-    );
-  }
+import { Button } from "antd";
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <Button onClick={() => setCount(count + 1)}>{count}</Button>
+    </>
+  );
 }
 
 export default App;
