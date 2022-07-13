913efaec2f470b4975fd59eec33dffe5ce60052c
qiuqianaw <qiuqianaw@163.com>
2022-07-13 18:15:49 +0800

    feature: useState初始值只在首次渲染生效，后续只要调用setState，整个app中的代码都会执行，此时的count每次拿到的都是最新值

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-// useState
-// 1. 倒入 useState
-// 2. 传入初始值
-// 3. [数据, 修改数据的方法]
-// 4. 使用，修改数据
+// 首次渲染
+// 首次被渲染的时候，组件内部的代码会被执行一次
+// 其中useState也会执行，初始值只在首次渲染时生效
+
+// 更新渲染 setCount都会更新
+// 1. app组件会再次渲染，这个函数会再次执行
+// 2. useState再次执行，得到新的count值不是0，而是修改之后的1，模版会用新值渲染
 
 import { Button } from "antd";
 import { useState } from "react";
