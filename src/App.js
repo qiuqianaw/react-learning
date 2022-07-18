42192d3a016818189348f0377e03a8ab1c780900
qiuqianaw <qiuqianaw@163.com>
2022-07-18 14:34:21 +0800

    feature: useEffect 副作用和基础使用

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-// 首次渲染
-// 首次被渲染的时候，组件内部的代码会被执行一次
-// 其中useState也会执行，初始值只在首次渲染时生效
-
-// 更新渲染 setCount都会更新
-// 1. app组件会再次渲染，这个函数会再次执行
-// 2. useState再次执行，得到新的count值不是0，而是修改之后的1，模版会用新值渲染
-
 import { Button } from "antd";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+// 1. 修改数据后，把count放在标题(副作用)
+// 2. 在函数组件中执行，传入回调，定义副作用
+// 3. 通过修改状态更新组件，副作用也会不断执行
+
+function updateTitle(count) {
+  document.title = count;
+}
+
 function App() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    updateTitle(count);
+  });
   return (
     <>
-      <Button onClick={() => setCount(count + 1)}>{count}</Button>
+      <div>
+        <Button onClick={() => setCount(count + 1)}>{count}</Button>
+      </div>
     </>
   );
 }
