3f6455aa1c010982bde909718598a8ebbfea7475
qiuqianaw <qiuqianaw@163.com>
2022-07-21 17:00:04 +0800

    feature: useEffect发送网络请求

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { Button } from "antd";
-import { useEffect, useState } from "react";
+// 类组件 发送网络请求
+// 生命周期 狗子函数 componentDidMount
+// 执行时机： 在初始化时DOM渲染完毕后执行一次
 
-function Test() {
-  useEffect(() => {
-    let timer = setInterval(() => {
-      console.log("定时器执行了");
-    }, 1000);
-    return () => {
-      // 清理
-      clearInterval(timer);
-    };
-  }, []);
-  return <div>This is Test</div>;
-}
+import { useEffect } from "react";
+
+// useEffect
+// 1. 不加依赖项： 初始化，重新渲染
+// 2. []： 只会在初始化执行一次
+// 3. 特定依赖项[count, name]： 首次执行，任意一个数值变化执行
+
 function App() {
-  const [flag, setFlag] = useState(true);
+  useEffect(() => {
+    function loadDate() {
+      fetch("http://geek.itheima.net/v1_0/channels")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+    loadDate();
+  }, []);
   return (
     <>
-      <div>
-        {flag ? <Test /> : null}
-        <Button onClick={() => setFlag(!flag)}>switch</Button>
-      </div>
+      <div></div>
     </>
   );
 }
