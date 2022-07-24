b6f61f9efcd873e04c20a4fe669538d65c72c3f3
qiuqianaw <qiuqianaw@163.com>
2022-07-24 11:11:28 +0800

    feature: react router demo

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    // 声明当前需要非hash模式路由
     <BrowserRouter>
+      {/* 指定跳转的组件 to用来配置路由地址 */}
       <Link to="/">首页</Link>
       <Link to="/about">关于</Link>
+      {/* 路由出口 路由对应组件在这里渲染 */}
       <Routes>
+        {/* 指定路径和组件的对应关系 */}
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
